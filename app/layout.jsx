93cdbcaff1ec7032752df4e7ef7e56bd6ff3b940
Roohan Khan <roohank8@gmail.com>
2023-09-24 05:44:56 +0500

    .

---
+++ app/layout.jsx
@@ -0,0 +1,28 @@
+import '@/styles/globals.css'
+import Navbar from '@/components/Navbar'
+import Provider from '@/components/Provider'
+
+export const metadeta={
+    title:'Promptopia',
+    description:'Discover & Share AI Prompts'
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang='en'>
+      <body>
+        <Provider>
+        <div className='main'>
+            <div className='gradient'/>
+        </div>
+        <main className='app'>
+            <Navbar/>
+            {children}
+        </main>
+        </Provider>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
